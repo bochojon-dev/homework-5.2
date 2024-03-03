6fc20000c964c817961d15baa88517497185b835
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-03-03 21:30:15 +0500

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// // 1-masala
+// function multiply(number) {
+//   let length = Math.abs(number).toString().length;
+//   return number * 5 ** length;
+// }
+// console.log(multiply(10));
+// console.log(multiply(0));
+// console.log(multiply(-1));
+
+// // 2-masala
+// function logs(x, a, b) {
+//   return (Math.log(a) + Math.log(b)) / Math.log(x);
+// }
+// console.log(logs(2, 2, 4));
+
+// // 3-masala
+// function sumArray(array) {
+//   if (!array || array.length <= 1) {
+//     return 0;
+//   }
+//   let sortedArr = array.sort((a, b) => a - b);
+//   return sortedArr.slice(1, -1).reduce((acc, num) => acc + num, 0);
+// }
+// console.log(sumArray([1, 2, 3, 4, 5, 5, 6]));
+
+// 4-masala
